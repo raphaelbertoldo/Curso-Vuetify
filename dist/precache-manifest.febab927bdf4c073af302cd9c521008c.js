ba9681e7460f8c7d84c51a59bd6b46ff9f9a81df
Raphael <rxphxel7k@gmail.com>
2022-02-23 18:08:09 -0300

    upload do projeto

---
+++ dist/precache-manifest.febab927bdf4c073af302cd9c521008c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ab5a322e5f4c962e848",
+    "url": "/css/chunk-vendors.2eb572a8.css"
+  },
+  {
+    "revision": "fe206ca3073a990f99114d99e0e715c0",
+    "url": "/img/bg.fe206ca3.jpg"
+  },
+  {
+    "revision": "1eb1a8444380458979c34751571d48fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9626d2313541002f91cc",
+    "url": "/js/app.31481839.js"
+  },
+  {
+    "revision": "7ab5a322e5f4c962e848",
+    "url": "/js/chunk-vendors.ac800080.js"
+  },
+  {
+    "revision": "fbafddbca0454e2cf83c4640f9391b35",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
